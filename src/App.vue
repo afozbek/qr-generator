5573959d6e6aaac042e30d4ae2e320aa8554fb18
afozbek <furkanozbek1995@gmail.com>
2020-07-22 22:23:52 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app-container">
+    <b-field
+      label="Enter Your Url Here:"
+      :type="inputType"
+      :message="errorMessage"
+    >
+      <b-input
+        type="url"
+        v-model="url"
+        placeholder="Geçerli bir url giriniz."
+      ></b-input>
+    </b-field>
+
+    <b-button @click="handleUrlSubmit" type="is-primary" outlined expanded
+      >Outlined</b-button
+    >
+
+    <img v-if="isUrlIsValid && isCalled" :src="callUrl" alt="QR Kodunuz" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      hasError: false,
+      errorMessage: "",
+      url: "https://google.com.tr",
+      baseApiUrl: "https://qrtag.net/api/qr_12.svg?url=",
+      callUrl: "",
+      isCalled: false,
+      urlRegex:
+        "(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])",
+    };
+  },
+  computed: {
+    inputType() {
+      return this.hasError ? "is-error" : "is-primary";
+    },
+    isUrlIsValid() {
+      console.log(new RegExp(this.urlRegex).test(this.url));
+      return new RegExp(this.urlRegex).test(this.url);
+    },
+  },
+  methods: {
+    async handleUrlSubmit() {
+      console.log("Hello World");
+
+      this.isCalled = true;
+      this.callUrl = this.baseApiUrl + this.url;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app-container {
+  width: 400px;
+  margin: 0 auto;
+
+  padding-top: 100px;
 }
 </style>
